92fdb25c7436e2ec94bf316780a73096d9928b75
unknown <sayed45@gmail.com>
2023-03-31 14:52:00 +0200

    add all files

---
+++ Tampelate One/main.js
@@ -0,0 +1,9 @@
+let links = document.querySelector(".links");
+let ul  =document.querySelector(".links ul");
+
+console.log(ul);
+
+// links.onclick = function () {
+//     ul.style.dispaly = "block";
+//     // console.log("yed");
+// }
